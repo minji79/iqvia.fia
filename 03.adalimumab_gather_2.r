bde4fc644cc5441ac480f8b45a69d5d9ef577f15
Minji_Kim <32572797+minji79@users.noreply.github.com>
2025-08-18 12:16:29 -0400

    Update 03.adalimumab_gather_2.r

--- 03.adalimumab_gather_2.r
+++ 03.adalimumab_gather_2.r
@@ -38,62 +38,16 @@ fwrite(ndc_list, file.path(data_dir, "ADALIMUMAB_NDCs.dta"))
 # 2. LOAD & COMBINE RAW CLAIMS FOR MULTIPLE YEARS: Many Ways to Do this. 
 # ------------------------------------------------------
 
-# 2.1. Attempt A with dta files 
-# This is the most complete, and it retains all varaibles in RXFACT. It takes the longest (by far)
-# Note the every other year is fine, because each file contains the year before data too
-
-# Time tracking
-cat("Start time:", format(Sys.time(), "%H:%M:%S"), "\n")
-
-# Get all 4 big files (100GB each)
-full_files <- c(
-  "/dcs07/hpm/data/iqvia_fia/full_raw/RxFact2018.fst",
-  "/dcs07/hpm/data/iqvia_fia/full_raw/RxFact2020.fst",
-  "/dcs07/hpm/data/iqvia_fia/full_raw/RxFact2022.fst",
-  "/dcs07/hpm/data/iqvia_fia/full_raw/RxFact2024.fst"
-)
-claims_all <- rbindlist(lapply(full_files, read_fst), use.names = TRUE, fill = TRUE) # Read and combine all 4 years
-
-# Rename daw_cd
-setnames(claims_all, old = "daw_cd", new = "daw_cd_s", skip_absent = TRUE)
-
-# Merge with a variable that was missing in our first cut of this data: encnt_outcm_cd 
-outcm <- read_parquet("/dcs07/hpm/data/iqvia_fia/full_raw/LevyPDRJRV.parquet") %>% as.data.table()
-
-# Keep only claim_id + encnt_outcm_cd
-outcm <- outcm[, .(claim_id, encnt_outcm_cd)]
-
-# Merge 1:1 by claim_id
-setkey(claims_all, claim_id)
-setkey(outcm, claim_id)
-claims_all <- merge(claims_all, outcm, by = "claim_id", all.x = TRUE)
-
-
-ndc_path <- "/dcs07/hpm/data/iqvia_fia/tutorial/gather_by_drug/r/data/ADALIMUMAB_NDCs.csv"
-ndcs <- fread(ndc_path)
-setnames(ndcs, old = "product_ndc", new = "ndc", skip_absent = TRUE) # Ensure correct column name
-setnames(claims_all, old = "ndc", new = "product_ndc", skip_absent = TRUE) # Standardize column in claims
-
-# Merge m:1 by product_ndc
-setkey(claims_all, product_ndc)
-setkey(ndcs, ndc)
-claims_adali <- merge(claims_all, ndcs, by.x = "product_ndc", by.y = "ndc", all.x = FALSE, all.y = FALSE)
-
-# Save as Parquet
-out_path <- "/dcs07/hpm/data/iqvia_fia/tutorial/gather_by_drug/r/data/A_ADALIMUMAB_claims.parquet"
-write_parquet(claims_adali, out_path)
-
-cat("End time:", format(Sys.time(), "%H:%M:%S"), "\n")
-
-
-# 2.2. Attempt B — Use pre-reduced version
-claims_small <- read_parquet("/dcs07/hpm/data/iqvia_fia/reduced/RxFact_2018_2024_small.fst") %>% as.data.table()
+# following Attempt B — Use pre-reduced version
+# Here I start with a reduced dataset I have already made, it contains all claims, including the encnt_outcm_cd variable, but way fewer variables (for efficnecy)
+claims_small <- read_fst("/dcs07/hpm/data/iqvia_fia/reduced/RxFact_2018_2024_small.fst") %>% as.data.table()
 
 ndcs <- fread(file.path(data_dir, "ADALIMUMAB_NDCs.dta"))$product_ndc
 
 claims_adali <- claims_small[product_ndc %in% ndcs]
 write_parquet(claims_adali, file.path(data_dir, "ADALIMUMAB_claims.parquet"))
 
+cat("End time:", format(Sys.time(), "%H:%M:%S"), "\n")
 
 # ------------------------------------------------------
 # 3. Add Rejection Group (rjct_grp) Classification
